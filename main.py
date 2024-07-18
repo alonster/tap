7b17299f507335c2f8c3a7711d52b0d0ba5ed885
Alon <alon02st@gmail.com>
2024-07-18 21:20:47 +0300

    Add context.

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
 import demo.cli
+from click import Context
 
 
 OUTPUT_PATH = './docs-output'
 
+command_template = """
+# {title}
 
-def generate_command(command):
+{description}
+
+## CLI Usage
+
+```
+{cli_usage}
+```
+
+## Options and Flags
+
+{options}
+"""
+
+
+def generate_command(command, context_path: list[Context]):
+    parent = context_path[-1] if context_path else None
+    context = Context(command, info_name=command.name, parent=parent)
     params = '\n'.join([f' - {param.name}' for param in command.params])
     with open(f'{OUTPUT_PATH}/{command.name}.md', 'w') as f:
-        f.write(f'# {command.name}\n\n{command.help}\n\n## Usage\n\n{params}')
+        f.write(
+            command_template.format(
+                title=command.name,
+                description=command.help,
+                cli_usage=command.get_usage(context),
+                options=params
+            )
+        )
 
 
-def generate_recursive(command):
-    generate_command(command)
+def generate_recursive(command, context_path: list[Context]):
+    parent = context_path[-1] if context_path else None
+    context = Context(command, info_name=command.name, parent=parent)
+    generate_command(command, context_path)
     if hasattr(command, 'commands'):
         for sub_name, sub_command in command.commands.items():
-            generate_recursive(sub_command)
+            generate_recursive(sub_command, context_path + [context])
 
 
 def main():
     # User defined - can be defaulted to 'cli'
     cli_super = demo.cli.cli
-    return generate_recursive(cli_super)
+    return generate_recursive(cli_super, [])
 
 
 if __name__ == '__main__':
